ba26ba1d8a6c438b81e8fee7e79b5b6599d025ba
Ana Perisic <anaperisictb@yahoo.com>
2022-05-13 13:30:17 +0200

    feature[extern endpoints]: example of creating new sprint board

--- scrum.py
+++ scrum.py
@@ -7,15 +7,28 @@ class Scrum(object):
     def interpret(self, model):
         for sprint in model.sprints:
             #print(model.__dict__)
+
+            query2 = {
+                    'idBoard':'627c210aa0ed4a48c3dd069c',
+                    'key': "9519ec4ca00591297f8bb4e7e184a841",
+                    'token': "013c3b97e0290d108573fb6d150a8bf32982b84150c20a4d372bf701dabe8d82",
+                    'name': sprint.name,
+                    
+                    
+            }
+            create_new_sprint(query2)
             for user_story in sprint.userStories: 
                 query = {
                     'idList': '627c210aa0ed4a48c3dd069d',
                     'key': "9519ec4ca00591297f8bb4e7e184a841",
                     'token': "013c3b97e0290d108573fb6d150a8bf32982b84150c20a4d372bf701dabe8d82",
                     'name': user_story.name,
-                    'desc': user_story.userStoryBody.storyAcceptanceCriteria.value
+                    'desc': user_story.userStoryBody.storyDescription.value,
+                    
                 }
+                
                 create_new_ticket(query)
+               
 
 #Extracting all the cards in all boards FROM TRELLO:
 def extracte_all_cards_from_all_boards():
@@ -30,7 +43,7 @@ def extracte_all_cards_from_all_boards():
         url_board_cards = "https://api.trello.com/1/boards/" + board_id +"/cards"
         response_board_cards = requests.request("GET", url_board_cards, params=querystring)
         data_board_cards = json.loads(response_board_cards.text)
-        print(data_board_cards)
+        #print(data_board_cards)
 
 #Create new card on TRELLO:
 def create_new_ticket(query):
@@ -46,7 +59,20 @@ def create_new_ticket(query):
         headers=headers,
         params=query
     )
+#Create new list on TRELLO:
+def create_new_sprint(query):
+    url="https://api.trello.com/1/lists"
 
+    headers= {
+        "Accept":"application/json"
+    }
+
+    response = requests.request(
+        "POST",
+        url,
+        headers=headers,
+        params=query
+    )
 
 def main():
 
