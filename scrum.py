b189633ad1f84a633c84c9ca6977edfa4d4b40b6
Ana Perisic <anaperisictb@yahoo.com>
2022-05-14 12:20:22 +0200

    refactor[extern endpoints]: moved other functions to the scrum methods

--- scrum.py
+++ scrum.py
@@ -3,13 +3,17 @@ from os.path import join, dirname
 from textx import metamodel_from_file
 import requests
 
+# Right now, we have a methods in this class that can create new sprint
+# on the Trello & also we can create tickets/stories in the new column
+# ie. list (and that represent a sprint)
+# TODO: Improve a class to support many tracking system
 class Scrum(object):
     def interpret(self, model):
         for sprint_model in model.sprints:
             #print(model.__dict__)
 
-            created_sprint = self.create_new_sprint(sprint_model)
-            self.create_sprint_user_stories(sprint_model.userStories, created_sprint)
+            created_sprint_on_trello = self.create_new_sprint_on_trello(sprint_model)
+            self.create_sprint_user_stories(sprint_model.userStories, created_sprint_on_trello)
 
 
     def create_sprint_user_stories(self, sprint_user_stories, created_sprint):
@@ -22,11 +26,9 @@ class Scrum(object):
                     'desc': user_story.userStoryBody.storyDescription.value,
                 }
                 
-            create_new_ticket(story_payload)
+            self.create_new_ticket_on_trello(story_payload)
 
-
-    # Create new list/column on TRELLO:
-    def create_new_sprint(self, sprint):
+    def create_new_sprint_on_trello(self, sprint):
 
         sprint_payload = {
                 'idBoard':'627c210aa0ed4a48c3dd069c',
@@ -50,8 +52,23 @@ class Scrum(object):
 
         return json.loads(response.text)
                
+    def create_new_ticket_on_trello(self, story_payload):
+        url = "https://api.trello.com/1/cards"
 
-#Extracting all the cards in all boards FROM TRELLO:
+        headers = {
+            "Accept": "application/json"
+        }
+
+        response = requests.request(
+            "POST",
+            url,
+            headers=headers,
+            params=story_payload
+        )
+
+
+
+# Extracting all the cards in all boards FROM TRELLO:
 def extracte_all_cards_from_all_boards():
     url_member = "https://api.trello.com/1/members/malibajojszd"
     querystring = {"key":"9519ec4ca00591297f8bb4e7e184a841","token":"013c3b97e0290d108573fb6d150a8bf32982b84150c20a4d372bf701dabe8d82"}
@@ -65,22 +82,6 @@ def extracte_all_cards_from_all_boards():
         response_board_cards = requests.request("GET", url_board_cards, params=querystring)
         data_board_cards = json.loads(response_board_cards.text)
         #print(data_board_cards)
-
-#Create new card on TRELLO:
-def create_new_ticket(query):
-    url = "https://api.trello.com/1/cards"
-
-    headers = {
-        "Accept": "application/json"
-    }
-
-    response = requests.request(
-        "POST",
-        url,
-        headers=headers,
-        params=query
-    )
-
 
 
 def main():
